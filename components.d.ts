ce3e58e93940ec39954b8383dbd31e85df77d74e
Егор Пантелеев <kakeparake@gmail.com>
2024-03-04 13:31:48 +0300

    config: add netlify deploy files

--- components.d.ts
+++ components.d.ts
@@ -7,14 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    Bar: typeof import('./src/components/charts/Bar.vue')['default']
     BarChart: typeof import('./src/components/charts/BarChart.vue')['default']
-    Doughnut: typeof import('./src/components/charts/Doughnut.vue')['default']
     DoughnutChart: typeof import('./src/components/charts/DoughnutChart.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElInput: typeof import('element-plus/es')['ElInput']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
