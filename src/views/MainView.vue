04b5b2355d0f818d0315e98c776631fcde013281
Егор Пантелеев <kakeparake@gmail.com>
2024-03-04 21:26:37 +0300

    feat: button for search

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,14 +5,24 @@
     <div class="text-center">
       <span class="text-[25px] md:text-[40px]"> Explore Github profiles </span>
     </div>
-    <div class="w-full flex justify-center">
+    <div class="w-full flex justify-center items-center flex-col gap-[30px]">
       <input
+        id="inputEl"
         v-model="inputValue"
-        class="w-[300px] rounded-[12px] text-[20px] transition duration-200 h-[50px] hover:scale-[1.01] md:w-[500px]"
+        class="w-[300px] rounded-[12px] text-[20px] transition duration-200 h-[50px] focus:bg-black/5 focus:ring-0 border-[1px] shadow-md focus:border-black md:w-[500px]"
         placeholder="Profile name"
         type="text"
         @keyup.enter="onSearch"
       />
+      <label
+        @click="onSearch"
+        for="inputEl"
+        class="bg-black cursor-pointer flex justify-center items-center gap-[5px] text-white p-[10px] hover:scale-[1.05] transition duration-200 rounded-[8px]"
+      >
+        <span>GO TO PROFILE</span>
+
+        <div class="mdi mdi-arrow-down-thick rotate-[-90deg]" />
+      </label>
     </div>
   </div>
 </template>
